fbc3f370e9ab0b486e3b34b9d6321f34f0aa9562
Parables <parables95@gmail.com>
2022-02-23 20:06:01 +0000

    last checkpoint...

---
+++ src/lib/components/side_menu.svelte
@@ -0,0 +1,35 @@
+<script>
+	import MenuItem from '$lib/components/menu_item.svelte';
+	import IconDashboard from '$lib/assets/svg/category.svg';
+	import IconFolder from '$lib/assets/svg/folder-open.svg';
+	import IconCalendar from '$lib/assets/svg/calendar.svg';
+	import IconChart from '$lib/assets/svg/chart-square.svg';
+	import IconTicket from '$lib/assets/svg/ticket-star.svg';
+	import IconSms from '$lib/assets/svg/sms.svg';
+	import IconSettings from '$lib/assets/svg/setting.svg';
+	import IconImport from '$lib/assets/svg/import.svg';
+	import IconLogout from '$lib/assets/svg/logout.svg';
+</script>
+
+<nav class="px-10 py-5 pr-0 w-2/12 bg-ghost-white-500">
+	<h1 class="text-2xl font-semibold p-4 mb-5">Educa<span class="text-indigo-500">me</span></h1>
+	<dl>
+		<MenuItem label="Dashboard" active>
+			<IconDashboard />
+		</MenuItem>
+		<MenuItem label="Class"><IconFolder /></MenuItem>
+		<MenuItem label="Schedule"><IconCalendar /></MenuItem>
+		<MenuItem label="Groups" badgeText="New"><IconChart /></MenuItem>
+		<MenuItem label="Grade"><IconTicket /></MenuItem>
+		<MenuItem label="Message" badgeText="2" badgeClass="bg-orange-200 text-orange-500"
+			><IconSms /></MenuItem
+		>
+		<MenuItem label="Settings"><IconSettings /></MenuItem>
+	</dl>
+
+	<div class="bg-white w-full rounded-md flex flex-col mr-10">
+		<span class="text-pink-500 bg-pink-300 rounded-full w-10 grid place-items-center h-10">
+			<IconImport />
+		</span>
+	</div>
+</nav>
