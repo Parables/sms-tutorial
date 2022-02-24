6d1feaf7fcc8ff1d5fb8c655aa70b3bf1e54e87b
Parables <parables95@gmail.com>
2022-02-24 20:05:21 +0000

    switched from the home/_ _layout.svelte to the root __layout.svelte

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,14 @@
 <script>
 	import '../app.css';
+	import SideMenu from '$lib/components/side_menu.svelte';
 </script>
 
-<slot />
+<div class=" flex items-stretch w-full h-full">
+	<SideMenu />
+
+	<main class="bg-ghost-white-900  w-full">
+		<slot />
+	</main>
+
+	<aside class="bg-ghost-white-700  w-3/12 " />
+</div>
